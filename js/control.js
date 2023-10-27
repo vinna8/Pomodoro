0506dde4bfaadd36bae114839eb79e1f212b9e60
vinna8 <vdovina.innusja@mail.ru>
2023-10-27 16:13:33 +0700

    navigation button active

--- js/control.js
+++ js/control.js
@@ -3,7 +3,20 @@ import { showTimer, startTimer } from "./timer.js";
 
 const btnStart = document.querySelector('.control__btn_start');
 const btnStop = document.querySelector('.control__btn_stop');
+const navigationBtns = document.querySelectorAll('.navigation__btn');
 
+export const changeAtiveBtn = (dataUse) => {
+    state.status = dataUse;
+    state.timeLeft = state[state.status] * 60;
+    showTimer(state.timeLeft);
+    for (let i = 0; i < navigationBtns.length; i++) {
+        if (navigationBtns[i].dataset.use === dataUse) {
+            navigationBtns[i].classList.add('navigation__btn_active');
+        } else {
+            navigationBtns[i].classList.remove('navigation__btn_active');
+        }
+    }
+}
 
 export const initControl = () => {
     btnStart.addEventListener('click', () => {
@@ -25,6 +38,12 @@ export const initControl = () => {
         state.timeLeft = state[state.status] * 60;
         showTimer(state.timeLeft);
     });
+
+    for (let i = 0; i < navigationBtns.length; i++) {
+        navigationBtns[i].addEventListener('click', () => {
+            changeAtiveBtn(navigationBtns[i].dataset.use);
+        })
+    }
 
     showTimer(state.timeLeft);
 }
